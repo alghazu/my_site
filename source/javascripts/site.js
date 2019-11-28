243fdc40af6e80df5139af47225fb7d9c4817a98
Nader <alghazu@gmail.com>
2019-11-28 22:23:24 +0100

    added google analytics

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,9 @@
-// This is where it all goes :)
+// Global site tag (gtag.js) - Google Analytics
+async src="https://www.googletagmanager.com/gtag/js?id=UA-153572512-1"
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-153572512-1');
+
